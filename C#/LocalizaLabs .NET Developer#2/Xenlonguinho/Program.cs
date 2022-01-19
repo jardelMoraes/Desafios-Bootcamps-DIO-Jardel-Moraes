44ea365bccc99ebb4103bfed46e23e8ba61f1061
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-01-19 16:19:00 -0300

    desafioXenlonguinhoFinalizado

--- C#/LocalizaLabs .NET Developer#2/Xenlonguinho/Program.cs
+++ C#/LocalizaLabs .NET Developer#2/Xenlonguinho/Program.cs
@@ -23,7 +23,15 @@ namespace Xenlonguinho
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var testes = int.Parse(Console.ReadLine());
+                
+            for (int i = 0; i < testes; i++)
+            {
+    
+                var esferas = int.Parse(Console.ReadLine());
+                Console.WriteLine(esferas - Math.Floor(Math.Sqrt(esferas)));
+            }
+            
         }
     }
 }
