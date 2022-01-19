f0fa892380dc29b0afca4b9aefd6467351a47279
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-01-19 14:55:20 -0300

    desafioHoHoHoFinalizado

--- C#/LocalizaLabs .NET Developer#2/Ho-Ho-Ho/Program.cs
+++ C#/LocalizaLabs .NET Developer#2/Ho-Ho-Ho/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*Papai Noel está brincando com seus duendes para entretê-los durante a véspera do Natal. A brincadeira consiste nos elfos escreverem números em pedaços de papel e colocarem no gorro do Papai Noel. Após todos terminarem de colocar os números Noel sorteia um papel e aquele número representa quantos "Ho" o Noel deve falar.
+
+Seu trabalho é ajudar o Papai Noel montando um problema que mostre todos os "Ho" que ele deve falar dado o número sorteado.
+
+Entrada
+A entrada é composta por um único inteiro N (0 < N ≤ 106) representando quantos "Ho" serão falados por Noel.
+
+Saída
+A saída é composta por todos "Ho" que Papai Noel deve falar separados por um espaço. Após o último "Ho" deve ser apresentado um "!" encerrando o programa.*/
+
+
+using System;
 
 namespace Ho_Ho_Ho
 {
@@ -6,7 +17,18 @@ namespace Ho_Ho_Ho
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < N; i++) {
+                if (i< N-1) {
+                    Console.Write("Ho ");                  //Complete o código no espaço em branco
+                }
+                else{
+                    Console.WriteLine("Ho!");
+                }
+            }
+            
         }
     }
 }
