299c6824caa4cc0abbbc8560c49439362f03a1dc
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-01-19 15:17:45 -0300

    desafioXporYFilalizado

--- C#/LocalizaLabs .NET Developer#2/Dividindo X por Y/Program.cs
+++ C#/LocalizaLabs .NET Developer#2/Dividindo X por Y/Program.cs
@@ -6,7 +6,22 @@ namespace Dividindo_X_por_Y
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int limit = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++) 
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+                if (Y == 0) 
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else 
+                {
+                    var divisao = X/Y;
+                    Console.WriteLine(""+divisao.ToString("0.0") );
+                }
+            }
         }
     }
 }
