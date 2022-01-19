2fa4ef91b26eb19a7a7079f9d92d8eecd1b4b10f
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-01-19 16:02:30 -0300

    finalizadoDesafioEsfera

--- C#/LocalizaLabs .NET Developer#2/VolumeEsfera/Program.cs
+++ C#/LocalizaLabs .NET Developer#2/VolumeEsfera/Program.cs
@@ -1,4 +1,12 @@
-﻿
+﻿/*Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+
+Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++), assumem que o resultado da divisão entre dois inteiros é outro inteiro.
+
+Entrada
+O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio da esfera.
+
+Saída
+A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes e um espaço depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.*/
 
 using System;
 
@@ -8,7 +16,16 @@ namespace VolumeEsfera
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double pi, raio, volume;
+            pi = 3.14159;
+            //escreva o seu código
+            
+            raio = Math.Abs(double.Parse(Console.ReadLine()));
+
+            volume = (4/3.0)*pi*Math.Pow(raio,3);
+
+            Console.WriteLine("VOLUME = {0:0.000}", volume);
+
         }
     }
 }
