7edd76e138867c52c2fde540ad329d8c45e7bfb8
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-02-01 16:30:42 -0300

    CommitInicialDesafioGFT4Mes
    Iniciando Desafio (Mes) BootcampGFT Start #4 .NET

---
+++ C#/GFT Start #4 .NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
