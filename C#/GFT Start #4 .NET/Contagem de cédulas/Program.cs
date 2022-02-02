21d75cf7026cafd68c151d51ed2f5b3d9135db24
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-02-01 22:58:42 -0300

    CommitFinalDesafioContagemCedulas
    Commit de finalização do desafio de contagem de cédulas

--- C#/GFT Start #4 .NET/Contagem de cédulas/Program.cs
+++ C#/GFT Start #4 .NET/Contagem de cédulas/Program.cs
@@ -18,6 +18,34 @@ namespace ContagemCedulas
             Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
             resto = resto % 100;
 
+            nota = 50;
+            quociente = resto / 50;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % 50;
+
+            nota = 20;
+            quociente = resto / 20;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % 20;
+
+            nota = 10;
+            quociente = resto / 10;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % 10;
+
+            nota = 5;
+            quociente = resto / 5;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % 5;
+
+            nota = 2;
+            quociente = resto / 2;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % 2;
+
+            nota = 1;
+            Console.WriteLine($"{resto} nota(s) de R$ {nota},00");
+            
         }
     }
 }
