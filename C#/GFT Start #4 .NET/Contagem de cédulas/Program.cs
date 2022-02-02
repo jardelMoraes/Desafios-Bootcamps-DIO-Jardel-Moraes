e93fad99da0ff14ced43b6006b4d17d864de68ae
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-02-01 22:52:47 -0300

    CommitInicialContagemCedulas
    Commit inical desafio para contagem de cédulas

---
+++ C#/GFT Start #4 .NET/Contagem de cédulas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ContagemCedulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nota, quociente, resto;
+
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+
+            resto = n;
+
+            nota = 100;
+            quociente = resto / 100;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % 100;
+
+        }
+    }
+}
