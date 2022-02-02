2c550ccc9066ccdb5ca59c18fc898d9afea25061
Perkles <jardelmoraes@alunos.utfpr.edu.br>
2022-02-01 22:45:38 -0300

    CommitFinalSomaDePàres
    Commit de finalização do desafio Soma de Pares

---
+++ C#/GFT Start #4 .NET/Soma de Pares Consecutivos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SomaParesConsecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                int x = int.Parse(Console.ReadLine());
+                if(x!=0)
+                {
+                    if(x % 2 == 0)
+                    {
+                        Console.WriteLine($"{x+x+2+x+4+x+6+x+8}");
+                    }
+                    else
+                    {
+                        x=x+1;
+                        Console.WriteLine($"{x+x+2+x+4+x+6+x+8}");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
